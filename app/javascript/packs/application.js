afe398c92efb7ee1ec603ad763d498deb5c9a6de
mcmerdith <matthewmeredith775@gmail.com>
2021-01-07 20:29:14 -0500

    Added Foundation for UI, jQuery for scripting. Created basic layout

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,10 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+require('jquery');
+require('foundation-sites');
+
+$(() => {
+  Foundation.addToJquery($);
+  $(document).foundation();
+});
